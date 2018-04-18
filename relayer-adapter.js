560c94c16443b0c43fa58ba4f7a489842f8aa772
Alex Bazhanau <sayshayx@gmail.com>
2018-04-18 23:34:52 +0300

    Update relayer-adapter.js

--- relayer-adapter.js
+++ relayer-adapter.js
@@ -1,45 +1,49 @@
 function convertToRelayer(plexDebtOrder) {
-    return {
-        kernelAddress: plexDebtOrder.kernelVersion,
-        repaymentRouterAddress: plexDebtOrder.issuanceVersion,
-        principalAmount: plexDebtOrder.principalAmount,
-        principalTokenAddress: plexDebtOrder.principalToken,
-        debtorAddress: plexDebtOrder.debtor,
-        debtorFee: plexDebtOrder.debtorFee,
-        termsContractAddress: plexDebtOrder.termsContract,
-        termsContractParameters: plexDebtOrder.termsContractParameters,
-        expirationTime: new Date(plexDebtOrder.expirationTimestampInSec * 1000).toISOString(),
-        salt: plexDebtOrder.salt,
-        debtorSignature: plexDebtOrder.debtorSignature,
-        underwriterAddress: plexDebtOrder.underwriter,
-        underwriterRiskRating: plexDebtOrder.underwriterRiskRating,
-        underwriterFee: plexDebtOrder.underwriterFee,
-        underwriterSignature: plexDebtOrder.underwriterSignature,
-        relayerAddress: plexDebtOrder.relayer,
-        relayerFee: plexDebtOrder.relayerFee,
-        description: plexDebtOrder.description
-    };
+  return {
+    kernelAddress: plexDebtOrder.kernelVersion,
+    repaymentRouterAddress: plexDebtOrder.issuanceVersion,
+    principalAmount: plexDebtOrder.principalAmount,
+    principalTokenAddress: plexDebtOrder.principalToken,
+    debtorAddress: plexDebtOrder.debtor,
+    debtorFee: plexDebtOrder.debtorFee,
+    termsContractAddress: plexDebtOrder.termsContract,
+    termsContractParameters: plexDebtOrder.termsContractParameters,
+    expirationTime: new Date(plexDebtOrder.expirationTimestampInSec * 1000).toISOString(),
+    salt: plexDebtOrder.salt,
+    debtorSignature: plexDebtOrder.debtorSignature,
+    underwriterAddress: plexDebtOrder.underwriter,
+    underwriterRiskRating: plexDebtOrder.underwriterRiskRating,
+    underwriterFee: plexDebtOrder.underwriterFee,
+    underwriterSignature: plexDebtOrder.underwriterSignature,
+    relayerAddress: plexDebtOrder.relayer,
+    relayerFee: plexDebtOrder.relayerFee,
+    description: plexDebtOrder.description
+  };
 }
 
 JSON.stringify(convertToRelayer(
-    {
-        "kernelAddress": "0x02c1c8cc7e43c044d632b0c1cb017ae107a2b5c7",
-        "repaymentRouterAddress": "0x849c8f030fb0e83068890e04d3ac94e058212d43",
-        "principalAmount": 101000000000000000000,
-        "principalTokenAddress": "0xa46535643bc5fb4ad28093723803652924c8e396",
-        "debtorAddress": "0x001d51cdc8f4b378e136642ddb95dfc4ff6a4b72",
-        "debtorFee": 0,
-        "termsContractAddress": "0x04f962e11ca5e351d90cf8d3e004ed46d2e0813f",
-        "termsContractParameters": "0x010000000579a814e10a740000013d3020003000000000000000000000000000",
-        "expirationTime": "2018-05-03T18:19:03.000Z",
-        "salt": 0,
-        "debtorSignature": "\"{\\\"v\\\":27,\\\"r\\\":\\\"0xcff513a90df6a97e5dae57adbcbae7698982efd44a02ab18b8446f1d57de2c08\\\",\\\"s\\\":\\\"0x2f47b8a3ea174259d71b3ae4b42360569199a066fe722fb1d3717647907718c1\\\"}\"",
-        "underwriterAddress": "0x0000000000000000000000000000000000000000",
-        "underwriterRiskRating": 0,
-        "underwriterFee": 0,
-        "underwriterSignature": "\"{\\\"r\\\":\\\"\\\",\\\"s\\\":\\\"\\\",\\\"v\\\":0}\"",
-        "relayerAddress": "0x0000000000000000000000000000000000000000",
-        "relayerFee": 0,
-        "description": "test debt order"
-    }
+  {
+    "principalAmount": 100000000000000000000,
+    "principalToken": "0x173701aef9768c0d0944066e92ac98aa111fd035",
+    "termsContract": "0x88c58b43376865b2b29f87c1f481a8a041ce8146",
+    "termsContractParameters": "0x01000000056bc75e2d6310000000271030003000000000000000000000000000",
+    "kernelVersion": "0x12298e1d4006ba83d62bbade1c9f1b84bcb44143",
+    "issuanceVersion": "0x6a4e6cb8167770e2b53d160248eeae6db5dc9652",
+    "debtor": "0x001d51cdc8f4b378e136642ddb95dfc4ff6a4b72",
+    "debtorFee": 0,
+    "creditor": "0x0000000000000000000000000000000000000000",
+    "creditorFee": 0,
+    "relayer": "0x0000000000000000000000000000000000000000",
+    "relayerFee": 0,
+    "underwriter": "0x0000000000000000000000000000000000000000",
+    "underwriterFee": 0,
+    "underwriterRiskRating": 0,
+    "expirationTimestampInSec": 1526674997,
+    "salt": 0,
+    "debtorSignature": "{\"v\":28,\"r\":\"0x731806f51d828c3d8d962fd28ede7f0011a7e010a9992eca670b46ec26fe9514\",\"s\":\"0x2e89049a24aff7f04e386d564877454914cf8668cc69cb2fd4a8cde7a0efb00f\"}",
+    "creditorSignature": "{\"r\":\"\",\"s\":\"\",\"v\":0}",
+    "underwriterSignature": "{\"r\":\"\",\"s\":\"\",\"v\":0}",
+    "description": "description",
+    "principalTokenSymbol": "MKR"
+  }
 ))
